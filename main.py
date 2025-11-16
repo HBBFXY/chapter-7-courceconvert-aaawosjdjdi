379384169b3838d043798b70e65139560dc3f957
aaawosjdjdi <1115213530@qq.com>
2025-11-16 17:08:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-# 在这个文件中编写代码实现题目要求的功能
-import keyword  # 建议使用这个库处理关键字
+import keyword
+
 reserved_words = set(keyword.kwlist)
 
-# 以下内容自行完成
+with open('random_int.py', 'r') as f:
+    content = f.read()
+
+result = []
+for word in content.split():
+    if word.islower() and word not in reserved_words:
+        result.append(word.upper())
+    else:
+        result.append(word)
+
+converted_content = ' '.join(result)
+
+with open('converted_random_int.py', 'w') as f:
+    f.write(converted_content)
